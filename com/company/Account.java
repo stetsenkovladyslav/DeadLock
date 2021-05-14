651bec3af678e4811b7c2d48e7f4e11d55ba97fa
stetsenko.v.n <stetsenko.v.n@gmail.com>
2021-05-14 11:28:59 +0300

    DeadLock

---
+++ com/company/Account.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Account {
+
+    private final int id;
+
+    private int balance;
+
+    public Account(int accountId, int initialBalance) {
+        this.id = accountId;
+        this.balance = initialBalance;
+    }
+
+    public void deposit(final int amount) {
+        balance += amount;
+    }
+
+    public void withdraw(final int amount) {
+        balance -= amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
